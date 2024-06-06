8520ce05d615757484ae40b193a49221231ffdd8
Jayshree/ RWI496 <144518062+Jayshree180520@users.noreply.github.com>
2024-06-06 15:08:48 +0530

    Add files via upload

---
+++ StringName.java
@@ -0,0 +1,47 @@
+package com;
+
+public class StringName {
+
+	public static void main(String[] args) {
+//1.	
+		String name = "Jayshree";
+	/*
+        System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
+    */
+		
+//2.		
+		//String lstring = name.toLowerCase();
+	    // System.out.println(lstring);
+
+
+//3. 
+		 //String ustring = name.toUpperCase();
+	      // System.out.println(ustring);
+		
+//4.
+	/*
+		String nonTrimmedString ="       Harry       ";
+	      System.out.println(nonTrimmedString);
+	        String trimedString = nonTrimmedString.trim();
+	       System.out.println(trimedString);
+	*/
+		
+		 //System.out.println(name.substring(1));
+	      // System.out.println(name.substring(1,4));
+	       //System.out.println(name.replace('r', 'p'));
+	       
+	       
+	      // System.out.println(name.equals("Jayshree"));
+	       // System.out.println(name.equalsIgnoreCase("Jayshree"));
+   
+	        System.out.println("I am escape sequences \"  double quote");
+	        System.out.println("I am escape sequences \\ double quote");
+	        System.out.println("I am escape sequences \n  double quote");
+	        System.out.println("I am escape sequences \t  double quote");
+    
+		
+	}
+
+}
